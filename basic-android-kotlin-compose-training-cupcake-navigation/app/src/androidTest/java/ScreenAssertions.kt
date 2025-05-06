7866969c09243fa74921789b72c962af46bcbdb5
ZRE <chy853@gmail.com>
2025-05-06 13:17:11 +0800

    update

---
+++ basic-android-kotlin-compose-training-cupcake-navigation/app/src/androidTest/java/ScreenAssertions.kt
@@ -0,0 +1,7 @@
+import androidx.navigation.NavController
+import org.junit.Assert.assertEquals
+
+fun NavController.assertCurrentRouteName(expectedRouteName: String) {
+    assertEquals(expectedRouteName, currentBackStackEntry?.destination?.route)
+}
+
